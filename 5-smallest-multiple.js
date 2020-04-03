613324af6e1979231fa61b07369033b34eb3123a
Stephen <stephen@shodges.io>
2020-04-03 18:01:20 -0400

    formatting

--- 5-smallest-multiple.js
+++ 5-smallest-multiple.js
@@ -3,15 +3,9 @@ const areMultiples = a => c => a.every(n => c % n === 0)
 
 const TEST_NUMBER = 2520
 const TEST_LIST = buildArray(10)
-
-
 areMultiples(TEST_LIST)(TEST_NUMBER) //?
 
-
-
-
 const FINAL_LIST = buildArray(20);
-
 const isMultipleOfFinalList = areMultiples(FINAL_LIST)
 
 let done = false
