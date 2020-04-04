0f44fa6eb2ca233be3b3214d144c50145d2d4512
Stephen <stephen@shodges.io>
2020-04-04 19:49:13 -0400

    add labels to execution measurements

--- src/measure.solve.ts
+++ src/measure.solve.ts
@@ -1,6 +1,7 @@
 export const measure = (f: any) => (x: any) =>
 {
-    console.time()
-    console.log('solution', f(x))
-    console.timeEnd()
+    console.time('execution time')
+    console.log('solution:', f(x))
+    console.timeEnd('execution time')
+    console.log('')
 }
