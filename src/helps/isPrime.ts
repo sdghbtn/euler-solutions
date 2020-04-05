d67200bd70e8c96eafb68f2cf1d907c4bd867733
Stephen <stephen@shodges.io>
2020-04-04 23:20:22 -0400

    remove extraneous type from for loop

--- src/helps/isPrime.ts
+++ src/helps/isPrime.ts
@@ -4,7 +4,7 @@ export const isPrime = (n: number) =>
     if (n <= 3) return true;
     if (n % 2 === 0 || n % 3 === 0) return false;
 
-    for (let i: number = 5; i * i <= n; i += 6)
+    for (let i = 5; i * i <= n; i += 6)
     {
         if (n % i === 0 || n % (i + 2) === 0) return false;
     }
