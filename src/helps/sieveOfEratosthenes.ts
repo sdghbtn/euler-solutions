28ad1cc92bb75c4a9f616eae307cf7dd19858d33
Stephen <stephen@shodges.io>
2020-04-04 21:50:11 -0400

    sieve help

---
+++ src/helps/sieveOfEratosthenes.ts
@@ -0,0 +1,27 @@
+import { buildArray } from '../helps/buildArray'
+
+export const sieveOfEratosthenes = (x: number) =>
+{
+    const isPrime = new Array(x + 1).fill(true)
+    isPrime[0] = false
+    isPrime[1] = false
+
+    const primes = []
+
+    for (let i = 2; i <= x; i++)
+    {
+        if (isPrime[i] === true)
+        {
+            primes.push(i)
+
+            let n = 2 * i
+            while (n <= x)
+            {
+                isPrime[n] = false
+                n += i
+            }
+        }
+    }
+
+    return primes
+}
