6c8e290e63b87a0012ca0baf9bcabd2b6e5a1d40
Stephen <stephen@shodges.io>
2020-04-05 16:40:50 -0400

    update solve script to handle problems higher than single digit

--- util/problem-solve/solveProblem.ts
+++ util/problem-solve/solveProblem.ts
@@ -3,6 +3,6 @@ import { fork } from 'child_process'
 
 readdir('./src/problems/', (e, d) => {
     if (e) throw e
-    let problemName = d.filter(s => s.charAt(0) === process.argv[2])
+    let problemName = d.filter(s => s.split('-')[0] === process.argv[2])
     fork(`./src/problems/${problemName}/${problemName}.solve.ts`)
 })
