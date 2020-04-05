152acabb25a0409fb0740b95886d1387113c01d5
Stephen <stephen@shodges.io>
2020-04-05 10:54:14 -0400

    simplify is palindrome help

--- src/helps/isPalindrome.ts
+++ src/helps/isPalindrome.ts
@@ -1,8 +1,3 @@
-import { halveString } from './halveString'
 import { reverseString } from './reverseString'
 
-export const isPalindrome = (n: number) =>
-{
-    const halves: string[] = halveString(n.toString())
-    return reverseString(halves.pop()) === halves.pop()
-}
+export const isPalindrome = (n: number) => reverseString(n.toString()) === n.toString()
