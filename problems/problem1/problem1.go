387185821293bb7967b1f9fbecf557c6d9865d7b
Stephen Hodges <stephen@shodges.io>
2021-05-15 02:08:21 -0400

    loop once instead of twice

--- problems/problem1/problem1.go
+++ problems/problem1/problem1.go
@@ -1,15 +1,11 @@
 package problem1
 
 func Solve(x int) int {
-	var multiplesOfThreeOrFive []int
-	for i := 0; i < x; i++ {
+	sum := 0
+	for i := 3; i < x; i++ {
 		if i%3 == 0 || i%5 == 0 {
-			multiplesOfThreeOrFive = append(multiplesOfThreeOrFive, i)
+			sum += i
 		}
-	}
-	var sum int
-	for _, y := range multiplesOfThreeOrFive {
-		sum += y
 	}
 	return sum
 }
