9f7c90970abda6cadcbda47f0671f57a8c513d24
Stephen Hodges <stephen@shodges.io>
2021-05-15 02:15:45 -0400

    use last two ints rather than []int (wow peter is so clever)

--- problems/problem2/problem2.go
+++ problems/problem2/problem2.go
@@ -1,19 +1,14 @@
 package problem2
 
 func Solve(x int) int {
-	seq := []int{1, 2}
-	var y int
+	y := 2
+	z := 3
 	sum := 2
-	for y <= x {
-		yy := seq[len(seq)-2:]
-		y = yy[0] + yy[1]
-		if y > x {
-			break
+	for z < x {
+		if z%2 == 0 {
+			sum += z
 		}
-		seq = append(seq, y)
-		if y%2 == 0 {
-			sum += y
-		}
+		y, z = z, y+z
 	}
 	return sum
 }
