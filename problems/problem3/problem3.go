a49e7c9fe1d61fe6e6886de40e47bfdeb8d64ffd
Stephen Hodges <stephen@shodges.io>
2021-10-19 03:09:28 -0400

    solution for problem 3

--- problems/problem3/problem3.go
+++ problems/problem3/problem3.go
@@ -1,5 +1,32 @@
 package problem3
 
-func Solve(x int) int {
+import "math"
 
+func Solve(x int) int {
+	c := x
+	prime := 2
+
+	// divide original number by 2 as many times as possible to eliminate
+	// prime number 2 and all evens
+	for c%2 == 0 {
+		c /= 2
+	}
+
+	// all occurrences of 2 are eliminated; start with the next prime, 3
+	// limit is sqrt of x because that is the largest possible factor
+	// iterate by 2 to traverse odd numbers since we have eliminated all evens
+	for i := 3; i <= int(math.Sqrt(float64(x))); i += 2 {
+		for c%i == 0 {
+			if i > prime {
+				prime = i
+			}
+			c /= i
+		}
+	}
+
+	if c > prime {
+		prime = c
+	}
+
+	return prime
 }
