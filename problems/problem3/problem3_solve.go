219598b8b0fac0dcc7e79ba0d90ea3438e97471f
Stephen Hodges <stephen@shodges.io>
2021-10-19 02:43:23 -0400

    add number to solve call

--- problems/problem3/problem3_solve.go
+++ problems/problem3/problem3_solve.go
@@ -1,18 +1,18 @@
 package problem3
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func RunSolution() {
-    start := time.Now()
+	start := time.Now()
 
-    solution := Solve()
+	solution := Solve(600851475143)
 
-    duration := time.Since(start)
+	duration := time.Since(start)
 
-    fmt.Printf("Solution: %d", solution)
-    fmt.Println("\nTime to solve:")
-    fmt.Println(duration)
+	fmt.Printf("Solution: %d", solution)
+	fmt.Println("\nTime to solve:")
+	fmt.Println(duration)
 }
